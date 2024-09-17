995281293fa5099341c30c70b0bd7c836d7d20e6
IliyaKuzmanov <ilia.kuzmanov.a60@learn.telerikacademy.com>
2024-09-18 00:18:47 +0300

    finish about page and index html

--- src/main/java/com/example/smartgarage/controllers/MVC/ServiceMvcController.java
+++ src/main/java/com/example/smartgarage/controllers/MVC/ServiceMvcController.java
@@ -1,12 +1,17 @@
 package com.example.smartgarage.controllers.MVC;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/services")
 public class ServiceMvcController {
 
+    @GetMapping
+    public String showServices() {
+        return "services";
+    }
 
 }
 
